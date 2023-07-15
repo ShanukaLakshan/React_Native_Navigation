804060325c17ead78c05778b8cebba6028226012
ShanukaLakshan <lakshants.20@uom.lk>
2023-07-15 13:24:55 +0530

    Add Sign up screen

--- src/screens/auth/Register.js
+++ src/screens/auth/Register.js
@@ -1,21 +1,168 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  SafeAreaView,
+  TouchableOpacity,
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import {COLORS, ROUTES} from '../../constants';
+import {useNavigation} from '@react-navigation/native';
 
-const Register = () => {
+const Register = props => {
+  // const {navigation} = props;
+  const navigation = useNavigation();
+
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>Register</Text>
-      {/* Add Register Icon */}
+    <SafeAreaView style={styles.main}>
+      <View style={styles.container}>
+        <View style={styles.wFull}>
+          <View style={styles.row}>
+            <Text style={styles.brandName}>Register Here</Text>
+          </View>
+
+          <Text style={styles.RegisterContinueTxt}>
+            Add your details to continue
+          </Text>
+          <TextInput style={styles.input} placeholder="Email" />
+          <TextInput style={styles.input} placeholder="Password" />
+          <TextInput style={styles.input} placeholder="Confirm Password" />
+
+          <View style={styles.RegisterBtnWrapper}>
+            <LinearGradient
+              colors={[COLORS.gradientForm, COLORS.primary]}
+              style={styles.linearGradient}
+              start={{y: 0.0, x: 0.0}}
+              end={{y: 1.0, x: 0.0}}>
+              {/******************** Register BUTTON *********************/}
+              <TouchableOpacity
+                onPress={() => (
+                  navigation.navigate(ROUTES.LOGIN),
+                  console.log('Register Button Pressed')
+                )}
+                activeOpacity={0.7}
+                style={styles.RegisterBtn}>
+                <Text style={styles.RegisterText}>Sign Up</Text>
+              </TouchableOpacity>
+            </LinearGradient>
+          </View>
+        </View>
+
+        <View style={styles.footer}>
+          <Text style={styles.footerText}> Do you have an account? </Text>
+          {/******************** Login BUTTON *********************/}
+          <TouchableOpacity onPress={() => navigation.navigate(ROUTES.LOGIN)}>
+            <Text style={styles.signupBtn}>Log In</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
 
 export default Register;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  container: {
+    padding: 15,
+    width: '100%',
+    position: 'relative',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  brandName: {
+    fontSize: 42,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: COLORS.primary,
+    opacity: 0.9,
+  },
+  RegisterContinueTxt: {
+    fontSize: 21,
+    textAlign: 'center',
+    color: COLORS.gray,
+    marginBottom: 16,
+    fontWeight: 'bold',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: COLORS.grayLight,
+    padding: 15,
+    marginVertical: 10,
+    borderRadius: 5,
+    height: 55,
+    paddingVertical: 0,
+  },
+  // Register Btn Styles
+  RegisterBtnWrapper: {
+    height: 55,
+    marginTop: 12,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.4,
+    shadowRadius: 3,
+    elevation: 5,
+  },
+  linearGradient: {
+    width: '100%',
+    borderRadius: 50,
+  },
+  RegisterBtn: {
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: 55,
+  },
+  RegisterText: {
+    color: COLORS.white,
+    fontSize: 16,
+    fontWeight: '400',
+  },
+  forgotPassText: {
+    color: COLORS.primary,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginTop: 15,
+  },
+  // footer
+  footer: {
+    position: 'absolute',
+    bottom: 20,
+    textAlign: 'center',
+    flexDirection: 'row',
+  },
+  footerText: {
+    color: COLORS.gray,
+    fontWeight: 'bold',
+  },
+  signupBtn: {
+    color: COLORS.primary,
+    fontWeight: 'bold',
+  },
+  // utils
+  wFull: {
+    width: '100%',
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 20,
+  },
+  mr7: {
+    marginRight: 7,
+  },
+});
